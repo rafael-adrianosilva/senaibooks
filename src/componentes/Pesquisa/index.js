05979c13c5904194f6cd734d020bae7c4f434537
rafael-adrianosilva <rafael.a.silva64@aluno.senai.br>
2025-10-14 17:31:40 -0300

    Segundo Commit

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,6 +1,7 @@
 import Input from "../Input";
 import styled from "styled-components";
 import { useState } from "react";
+import { livros } from "./dadosPesquisa";
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -25,17 +26,49 @@ const Subtitulo = styled.h3`
     margin-bottom: 40px;
 `
 
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+
+    p{
+        width: 200px;
+    }
+    img{
+        width: 100px;
+    }
+    
+    &:hover{
+    border: 1px solid white;
+    }
+`
+
 function Pesquisa(){
-    const [textoDigitado, setTextoDigitado] = useState('');
+    const [livrosPesquisados, setLivrosPesquisados] = useState([]);
+    
+    console.log(livrosPesquisados);
     return (
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
             <Subtitulo>Encontre seu livro em nossa estante</Subtitulo>
             <Input 
             placeholder="Escreva sua próxima leitura:"
-            onBlur={evento => setTextoDigitado(evento.target.value)}
+            onInput={evento => {
+                const livrosPesquisados = evento.target.value
+                const resultadoPesquisa = livros.filter(livro => livro.nome.includes(livrosPesquisados))
+                setLivrosPesquisados(resultadoPesquisa)   
+            }}
             />
-            <p>{textoDigitado}</p>
+            { livrosPesquisados.map( livro => (
+              <Resultado>
+                <img src={livro.src}/>
+                <p>{livro.nome}</p>
+              </Resultado>  
+            ))}
+
+
         </PesquisaContainer>
     )
 }
